70db0255858ebaa14cf39649268dc3c759d5e163
6410110204 <6410110204@psu.ac.th>
2023-03-07 21:56:21 +0700

    calculator

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,13 +2,15 @@ import React from 'react'
 import NavBar from '../components/Tools/Navbar/Navbar'
 import Footer from '../components/Tools/Footer/Footer';
 import HomeCard from '../components/Data/Home/HomeCard';
-
+import Calculator from '../components/Data/Home/Calculator';
 function Home() {
   return (
     <div>
         <NavBar/>
+        <Calculator/>
         <HomeCard/>
         <Footer/>
+        <ca/>
     </div>
   )
 }
