9f5be37d2bd9613ab840e817f19e1c2ea2b9dd12
KixarS <teoylimhao2@gmail.com>
2023-03-08 01:00:31 +0700

    card calculator

--- src/components/Data/Home/Calculator.js
+++ src/components/Data/Home/Calculator.js
@@ -1,3 +1,4 @@
+import { Card } from "@mui/material";
 import React, { useState } from "react";
 import './Calculator.css';
 
@@ -8,42 +9,45 @@ function Calculator() {
   const [heightError, setHeightError] = useState('');
   const [weightError, setWeightError] = useState('');
   const [result, setResult] = useState('');
-  
+
 
   const calculateBMI = () => {
     // Validate input values
     const heightInMeters = height / 100;
-  const bmi = weight / (heightInMeters * heightInMeters);
-  let bmiClass;
+    const bmi = weight / (heightInMeters * heightInMeters);
+    let bmiClass;
 
-  if (bmi < 18.5) {
-    bmiClass = "ผอม";
-  } else if (bmi >= 18.5 && bmi <= 24.9) {
-    bmiClass = "น้ำหนักปกติ";
-  } else if (bmi >= 25 && bmi <= 29.9) {
-    bmiClass = "อ้วน";
-  } else {
-    bmiClass = "อ้วนเกินไป";
+    if (bmi < 18.5) {
+      bmiClass = "ผอม";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+      bmiClass = "น้ำหนักปกติ";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+      bmiClass = "อ้วน";
+    } else {
+      bmiClass = "อ้วนเกินไป";
+    }
+
+    setResult(`ค่า BMI ของคุณคือ : ${bmi.toFixed(1)} (${bmiClass})`);
   }
 
-  setResult(`ค่า BMI ของคุณคือ : ${bmi.toFixed(1)} (${bmiClass})`);
-}
-
   return (
-    <div className="App">
-      <div className="container">
-        <label htmlFor="height">ส่วนสูง (cm)</label>
-        <input type="number" id="height" name="height" value={height} onChange={(e) => setHeight(e.target.value)} required />
-        <div className="error">{heightError}</div>
+    <Card sx={{ mr: '30rem', ml: '30rem', mt: '25rem', bgcolor: 'white', borderRadius:'5rem'}}>
+      <div className="App">
+        <div className="container">
+          <label htmlFor="height">ส่วนสูง (cm)</label>
+          <input type="number" id="height" name="height" value={height} onChange={(e) => setHeight(e.target.value)} required />
+          <div className="error">{heightError}</div>
 
-        <label htmlFor="weight">น้ำหนัก (kg)</label>
-        <input type="number" id="weight" name="weight" value={weight} onChange={(e) => setWeight(e.target.value)} required />
-        <div className="error">{weightError}</div>
+          <label htmlFor="weight">น้ำหนัก (kg)</label>
+          <input type="number" id="weight" name="weight" value={weight} onChange={(e) => setWeight(e.target.value)} required />
+          <div className="error">{weightError}</div>
 
-        <button type="button" onClick={calculateBMI} >Calculate</button>
-        <div id="result">{result}</div>
+          <button type="button" onClick={calculateBMI} >Calculate</button>
+          <div id="result">{result}</div>
+        </div>
       </div>
-    </div>
+    </Card>
+
   );
 }
 
