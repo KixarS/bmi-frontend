46ed560bcab0a0c7e2488294c23cff550e9d57be
KixarS <teoylimhao2@gmail.com>
2023-03-08 17:23:22 +0700

    Analyze database

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,12 +7,6 @@ function Home() {
     <div>
       <NavBar />
       <HomeCard />
-      <iframe
-        allow="microphone;"
-        width="350"
-        height="430"
-        src="https://console.dialogflow.com/api-client/demo/embedded/85fd224e-ed13-478a-b57a-00aab549c7cc">
-      </iframe>
       <Footer />
     </div>
   )
